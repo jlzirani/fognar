e96bf412b5b69a6150b68ee264d77342da276757
Bouska <bouska@eskapa.be>
2012-09-08 17:44:38 +0200

    [mod] New script from teh internetz to convert ISO datetime into pretty dates. Translated and some mod from the original script.

--- static/third_party/humanedate.js
+++ static/third_party/humanedate.js
@@ -1,26 +1,111 @@
 /*
- * JavaScript Pretty Date
- * Copyright (c) 2011 John Resig (ejohn.org)
- * Licensed under the MIT and GPL licenses.
- */
+* Javascript Humane Dates
+* Copyright (c) 2008 Dean Landolt (deanlandolt.com)
+* Re-write by Zach Leatherman (zachleat.com)
+*
+* Adopted from the John Resig's pretty.js
+* at http://ejohn.org/blog/javascript-pretty-date
+* and henrah's proposed modification
+* at http://ejohn.org/blog/javascript-pretty-date/#comment-297458
+*
+* Licensed under the MIT license.
+*/
 
-// Takes an ISO time and returns a string representing how
-// long ago the date represents.
-function prettyDate(time){
-	var date = new Date((time || "").replace(/-/g,"/").replace(/[TZ]/g," ")),
-		diff = (((new Date()).getTime() - date.getTime()) / 1000),
-		day_diff = Math.floor(diff / 86400);
-			
-	if ( isNaN(day_diff) || day_diff < 0 || day_diff >= 31 )
-		return;
-			
-	return day_diff == 0 && (
-			diff < 60 && "just now" ||
-			diff < 120 && "1 minute ago" ||
-			diff < 3600 && Math.floor( diff / 60 ) + " minutes ago" ||
-			diff < 7200 && "1 hour ago" ||
-			diff < 86400 && Math.floor( diff / 3600 ) + " hours ago") ||
-		day_diff == 1 && "Yesterday" ||
-		day_diff < 7 && day_diff + " days ago" ||
-		day_diff < 31 && Math.ceil( day_diff / 7 ) + " weeks ago";
-}
+function prettyDate(date, compareTo){
+
+    if(!date) {
+        return;
+    }
+
+    var lang = {
+            ago: 'Il y a ',
+            from: '',
+            now: 'Maintenant',
+            minute: 'minute',
+            minutes: 'minutes',
+            hour: 'heure',
+            hours: 'heures',
+            day: 'jour',
+            days: 'jours',
+            week: 'semaine',
+            weeks: 'semaines',
+            month: 'mois',
+            months: 'mois',
+            year: 'année',
+            years: 'années'
+        },
+        formats = [
+            [60, lang.now],
+            [3600, lang.minute, lang.minutes, 60], // 60 minutes, 1 minute
+            [86400, lang.hour, lang.hours, 3600], // 24 hours, 1 hour
+            [604800, lang.day, lang.days, 86400], // 7 days, 1 day
+            [2628000, lang.week, lang.weeks, 604800], // ~1 month, 1 week
+            [31536000, lang.month, lang.months, 2628000], // 1 year, ~1 month
+            [Infinity, lang.year, lang.years, 31536000] // Infinity, 1 year
+        ],
+        isString = typeof date == 'string',
+        sdate = date,
+        date = isString ?
+                    new Date('' + date) :
+                    date,
+        compareTo = compareTo || new Date,
+        seconds = (compareTo - date
+                        //(compareTo.getTimezoneOffset() -
+                            // if we received a GMT time from a string, doesn't include time zone bias
+                            // if we got a date object, the time zone is built in, we need to remove it.
+                        //    (isString ? 0 : date.getTimezoneOffset())
+                        //) * 60000
+                    ) / 1000,
+        token;
+        console.log(sdate);
+        console.log(date + '' + compareTo)
+
+//    if(seconds < 0) {
+//        seconds = Math.abs(seconds);
+//        token = lang.from ? ' ' + lang.from : '';
+//    } else {
+//        token = lang.ago ? ' ' + lang.ago : '';
+//    }
+    token = lang.ago;
+
+    /*
+* 0 seconds && < 60 seconds Now
+* 60 seconds 1 Minute
+* > 60 seconds && < 60 minutes X Minutes
+* 60 minutes 1 Hour
+* > 60 minutes && < 24 hours X Hours
+* 24 hours 1 Day
+* > 24 hours && < 7 days X Days
+* 7 days 1 Week
+* > 7 days && < ~ 1 Month X Weeks
+* ~ 1 Month 1 Month
+* > ~ 1 Month && < 1 Year X Months
+* 1 Year 1 Year
+* > 1 Year X Years
+*
+* Single units are +10%. 1 Year shows first at 1 Year + 10%
+*/
+
+    function normalize(val, single)
+    {
+        var margin = 0.1;
+        if(val >= single && val <= single * (1+margin)) {
+            return single;
+        }
+        return val;
+    }
+
+    for(var i = 0, format = formats[0]; formats[i]; format = formats[++i]) {
+        if(seconds < format[0]) {
+            if(i === 0) {
+                // Now
+                return format[1];
+            }
+
+            var val = Math.ceil(normalize(seconds, format[3]) / (format[3]));
+            return (i > 0 ? token : '') + val +
+                    ' ' +
+                    (val != 1 ? format[2] : format[1]);
+        }
+    }
+};
